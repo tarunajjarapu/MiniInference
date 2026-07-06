1e2d898fe9524ca9115a8da2a02de68e43d4a8ea
Tarun Ajjarapu <tarunajj@gmail.com>
2026-07-05 23:41:20 -0700

    Loaded model and setup py env

--- src/loop.py
+++ src/loop.py
@@ -1,2 +1,13 @@
+from transformers import AutoTokenizer, AutoModelForCasualLM
+import torch
+
+model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, torch_dtype="auto", device_map="auto"
+)
+
+tokenizenizer = AutoTokenizer.from_pretrained(model_id)
+
 prompt = input("Enter prompt: ")
 print(prompt)
