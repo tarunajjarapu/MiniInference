87fb7c3d684ae7d1c66fdbead88a7802c8fae271
Tarun Ajjarapu <tarunajj@gmail.com>
2026-07-10 21:19:06 -0700

    Setup tokenizer and tested compliation

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_id, torch_dtype="auto", device_map="auto"
-)
+# model = AutoModelForCausalLM.from_pretrained(
+#   model_id, torch_dtype="auto", device_map="auto"
+# )
 
-tokenizenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 text = input("Enter Prompt: ")
 print(text)
-
-inputs = tokenizenizer(text, return_tensor="pt", padding=True, truncation=True)
+encoded = tokenizer.encode(text)
+print(encoded)
+print(tokenizer.decode(encoded))
+print(tokenizer.tokenize(text))
+# inputs = tokenizer(text, return_tensor="pt", padding=True, truncation=True)
