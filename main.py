3289f838504ed76645bb8162461a71baa0dc2322
Tarun Ajjarapu <tarunajj@gmail.com>
2026-07-11 23:00:33 -0700

    loaded in file through huggingface

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
-# model = AutoModelForCausalLM.from_pretrained(
-#   model_id, torch_dtype="auto", device_map="auto"
-# )
+model = AutoModelForCausalLM.from_pretrained(model_id)
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
