a568aeb9262ed34afceac9d6e03ed300619152db
Tarun Ajjarapu <tarunajj@gmail.com>
2026-07-13 17:30:13 -0700

    nn tensor linear layers

--- main.py
+++ main.py
@@ -19,4 +19,6 @@ for name, param in model.named_parameters():
     print(param.shape)
     print(param.dtype)
     print(param.device)
+
+print(nn.linear(2048, 256))
 # inputs = tokenizer(text, return_tensor="pt", padding=True, truncation=True)
