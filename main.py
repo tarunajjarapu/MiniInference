856c83b9410ad4b01d71fee294493418fb805c26
Tarun Ajjarapu <tarunajj@gmail.com>
2026-07-17 21:58:07 -0700

    Added all scheduled tasks to scheduler to batch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+from transformers.models.switch_transformers.modeling_switch_transformers import (
+    router_z_loss_func,
+)
+
+import scheduler
 
 model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
@@ -20,10 +25,17 @@ for name, param in model.named_parameters():
     print(param.dtype)
     print(param.device)
 
+user = input("User Name: ")
 next_request = input("Next Prompt or q to quit: ")
 all_requests = []
 while next_request != "q":
     all_requests.append(next_request)
     next_request = input("Next Prompt or q to quit")
+
+state = scheduler.Router
+scheduled_tasks = []
+for request in all_requests:
+    scheduled_tasks.append(scheduler.Request(user, tokenizer.tokenize(all_requests[i])))
+    router.schedule(scheduled_tasks)
 
 # inputs = tokenizer(text, return_tensor="pt", padding=True, truncation=True)
