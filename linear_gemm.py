e9d63ffafe98bb361410e4af6eee1675c52966cd
Tarun Ajjarapu <tarunajj@gmail.com>
2026-07-20 21:22:37 -0700

    Added a linear time gemm

---
+++ linear_gemm.py
@@ -0,0 +1,8 @@
+def multiply(A, B):
+    new_matrix = [[0 for col in range(len(B[0]))] for row in range(len(A))]
+    for row in range(len(A)):
+        for col in range(len(B[0])):
+            val = 0
+            for i in range(len(A[0])):
+                val += A[row][i] * B[i][col]
+            new_matrix[row][col] = val
